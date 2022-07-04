655c45d8ec73ca6dd90b6378a3a9127bd243846a
salsabildhn <salsabildhn@student.uns.ac.id>
2022-07-05 02:28:08 +0700

    first commit

---
+++ lib/model/model_niat.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/physics.dart';
+
+class ModelNiat {
+  int? id;
+  String? name;
+  String? arabic;
+  String? latin;
+  String? terjemahan;
+
+  ModelNiat(this.id, this.name, this.arabic, this.latin, this.terjemahan);
+
+  ModelNiat.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    arabic = json['arabic'];
+    latin = json['latin'];
+    terjemahan = json['terjemahan'];
+  }
+}
---
+++ lib/page/main_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:project_pab/page/niat_sholat_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Expanded(
+                child: InkWell(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => NiatSholat()));
+                  },
+                  child: Column(
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("assets/images/ic_niat.png"),
+                        height: 100,
+                        width: 100,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Niat Shalat",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Expanded(
+                child: InkWell(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {},
+                  child: Column(
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("assets/images/ic_doa.png"),
+                        height: 100,
+                        width: 100,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Bacaan Shalat",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Expanded(
+                child: InkWell(
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  onTap: () {},
+                  child: Column(
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("assets/images/ic_bacaan.png"),
+                        height: 100,
+                        width: 100,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Ayat Kursi",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
